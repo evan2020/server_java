87f4923ec95d3f308a94a509f38729f03df3f6f6
evan2020 <evan199101@gmail.com>
2018-09-22 05:48:03 +0800

    设置跨越

--- src/main/java/com/shalou/demo/DemoApplication.java
+++ src/main/java/com/shalou/demo/DemoApplication.java
@@ -2,9 +2,24 @@ package com.shalou.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+
 
 @SpringBootApplication
 public class DemoApplication {
+
+    //跨域处理
+    @Configuration
+    public class WebConfig extends WebMvcConfigurationSupport {
+
+        @Override
+        public void addCorsMappings(CorsRegistry registry) {
+            registry.addMapping("/**")
+                    .allowedMethods("HEAD", "GET", "PUT", "POST", "DELETE", "PATCH");
+        }
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
